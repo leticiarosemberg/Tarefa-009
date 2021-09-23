9dca779f3b9d1cb2c0efd9eb685926eb501ba140
Matheuspraticaprof <mathmonvale@gmail.com>
2021-09-23 19:07:50 +0000

    Update site

--- site/run.py
+++ site/run.py
@@ -1,7 +1,29 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
 app.debug = True
+
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template('about.html')
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro_page():
+    return render_template('cadastro.html')
+
+@app.route('/news')
+def news_page():
+    return render_template('news.html')
+
+@app.route('/songs')
+def songs_page():
+    return render_template('songs.html')
+
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
